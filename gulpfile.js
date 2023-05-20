d888384ecc452bab071dcd5c0b3da940054d3e29
Dani Matuko <matuko305@gmail.com>
2023-05-20 17:01:08 +0300

    remove dit from git tracking

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const sass = require("gulp-sass")(require("sass"));
 
 function compileSass() {
   return gulp
-    .src("index.scss")
+    .src("sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("dist/css"));
 }
 
 function watchSass() {
-  gulp.watch("index.scss", compileSass);
+  gulp.watch("sass/**/*.scss", compileSass);
 }
 
 exports.default = gulp.series(compileSass, watchSass);
