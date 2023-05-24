ea338ae946c663e20b4293e2ef524c15b5e02cf8
Dani Matuko <matuko305@gmail.com>
2023-05-24 13:32:03 +0300

    order html page and add typography partial

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function compileSass() {
   return gulp
     .src("sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(
+      purgecss({
+        content: ["*.html"],
+      })
+    )
     .pipe(gulp.dest("dist/css"));
 }
 
 function watchSass() {
-  gulp.watch("sass/**/*.scss", compileSass);
+  gulp.watch(["sass/**/*.scss", "*.html"], compileSass);
 }
 
 exports.default = gulp.series(compileSass, watchSass);
